7233c94a3a541138bb74517c7523a3d92ae9e24a
Julien <julien.moren0@outlook.fr>
2021-12-28 03:23:57 +0100

    optimisation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,38 @@
 function lancer() {
-    // Me retourne ou True ou False 1/2 
-    var q = (Math.floor(Math.random() * 2) === 0);
-
-
-
-    if (q == true) {
+    if (Math.floor(Math.random() * 2) === 0)
         document.getElementById("piece").innerHTML = '<img class="piece" id="coin" src="./assets/img/pile-nobg.png"><br><small id="description" class="commentaire">Pile</small>';
-
-    }
-    if (q != true) {
+    else
         document.getElementById("piece").innerHTML = '<img class="piece" id="coin" src="./assets/img/face-nobg.png"><br><small id="description" class="commentaire">Face</small>';
-    }
     document.getElementById('coin').className = 'piece';
 
+}
+
+var darkSwitch = document.getElementById("darkSwitch");
+window.addEventListener("load", function() {
+    if (darkSwitch) {
+        initTheme();
+        darkSwitch.addEventListener("change", function() {
+            resetTheme();
+        });
+    }
+});
+
+function initTheme() {
+    var darkThemeSelected =
+        localStorage.getItem("darkSwitch") !== null &&
+        localStorage.getItem("darkSwitch") === "dark";
+    darkSwitch.checked = darkThemeSelected;
+    darkThemeSelected ?
+        document.body.setAttribute("data-theme", "dark") :
+        document.body.removeAttribute("data-theme");
+}
+
+function resetTheme() {
+    if (darkSwitch.checked) {
+        document.body.setAttribute("data-theme", "dark");
+        localStorage.setItem("darkSwitch", "dark");
+    } else {
+        document.body.removeAttribute("data-theme");
+        localStorage.removeItem("darkSwitch");
+    }
 }
